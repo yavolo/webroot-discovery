55e6123c11fbfabfa4f27d4bd2fee6caa74aa12c
Oliver Rickfors <oliverrickfors@hotmail.com>
2019-03-23 17:20:27 +0100

    init

---
+++ webroot-discovery.py
@@ -0,0 +1,34 @@
+import glob
+import sys
+import re
+from termcolor import colored
+import urllib.request
+import requests
+
+regexlist = []
+urlList = []
+
+if len(sys.argv) >= 3:
+	wwwrootpath = sys.argv[1]
+	siteurl = sys.argv[2]
+	filetype = "*." + sys.argv[3]
+
+else:
+	print('Missing argument variables. wwwroot siteurl filetype(e.g. jsp, aspx, php)')
+	exit()
+
+filepaths = glob.iglob((wwwrootpath +"/**/"+ filetype), recursive=True)
+
+for filename in filepaths:
+	print (siteurl + (filename.replace(sys.argv[1], "")))
+
+yesorno = input('Send to proxy? y/n')
+
+if yesorno == 'y':
+	for filename in filepaths:
+		proxies = {
+			  "http": "http://127.0.0.1:8080",
+			  "https": "http://127.0.0.1:8080",
+			}
+
+		requests.get((siteurl + (filename.replace(sys.argv[1], ""))), proxies=proxies)
